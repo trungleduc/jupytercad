ae8de75a04d875bd16c27eb0b3e56508a34c2359
Duc Trung Le <leductrungxf@gmail.com>
2023-01-05 20:30:23 +0100

    Add and remove point

--- src/sketcher/sketchermodel.ts
+++ src/sketcher/sketchermodel.ts
@@ -9,11 +9,18 @@ export interface ILine {
   start: IPosition;
   end: IPosition;
 }
-
+function distance(p1: IPosition, p2: IPosition): number {
+  return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
+}
 export class SketcherModel {
-  constructor() {
+  constructor(options: { gridSize: number }) {
     this._points = new Map();
     this._lines = new Map();
+    this._gridSize = options.gridSize;
+  }
+
+  get gridSize(): number {
+    return this._gridSize;
   }
   get points(): Map<string, IPosition> {
     return this._points;
@@ -29,6 +36,14 @@ export class SketcherModel {
   removePoint(id: string): void {
     this._points.delete(id);
   }
+  getPointByPosition(pos: IPosition): string | undefined {
+    for (const [key, val] of this._points.entries()) {
+      if (distance(val, pos) < 0.05 * this._gridSize) {
+        return key;
+      }
+    }
+  }
   private _points: Map<string, IPosition>;
   private _lines: Map<string, ILine>;
+  private _gridSize: number;
 }
